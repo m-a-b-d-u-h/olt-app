023263ada9cf5f6edabcaa75513cade8ea20cf18
Muhammad Abduh <abduh@sellerpintar.com>
2026-05-25 10:04:14 +0700

    fix: clear disabled kex algorithms for Huawei OLT

--- olt_service.py
+++ olt_service.py
@@ -4,11 +4,12 @@ import time
 import paramiko
 from netmiko import ConnectHandler, NetmikoTimeoutException, NetmikoAuthenticationException
 
+paramiko.Transport._disabled_algorithms = {"kex": []}
 paramiko.Transport._preferred_kex = (
-    'diffie-hellman-group14-sha256',
-    'diffie-hellman-group14-sha1',
     'diffie-hellman-group1-sha1',
     'diffie-hellman-group-exchange-sha1',
+    'diffie-hellman-group14-sha1',
+    'diffie-hellman-group14-sha256',
     'diffie-hellman-group-exchange-sha256',
     'ecdh-sha2-nistp256',
     'ecdh-sha2-nistp384',
