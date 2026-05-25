edccef3ff57f83855410bcc7a8ee722e7a109453
Muhammad Abduh <abduh@sellerpintar.com>
2026-05-25 10:02:58 +0700

    fix: add legacy kex algorithms for Huawei OLT

--- olt_service.py
+++ olt_service.py
@@ -1,7 +1,20 @@
 import re
 import socket
 import time
+import paramiko
 from netmiko import ConnectHandler, NetmikoTimeoutException, NetmikoAuthenticationException
+
+paramiko.Transport._preferred_kex = (
+    'diffie-hellman-group14-sha256',
+    'diffie-hellman-group14-sha1',
+    'diffie-hellman-group1-sha1',
+    'diffie-hellman-group-exchange-sha1',
+    'diffie-hellman-group-exchange-sha256',
+    'ecdh-sha2-nistp256',
+    'ecdh-sha2-nistp384',
+    'ecdh-sha2-nistp521',
+    'curve25519-sha256@libssh.org',
+)
 
 def _get_netmiko_params(olt):
     return {
