dd43907aa8adbe43b78385d34ffe82cd0ac25cdd
Muhammad Abduh <abduh@sellerpintar.com>
2026-05-25 09:52:57 +0700

    change host to localhost

--- app.py
+++ app.py
@@ -474,4 +474,4 @@ def handle_disconnect():
             pass
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=3000, debug=True)
+    socketio.run(app, host='127.0.0.1', port=3000, debug=True)
